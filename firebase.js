b0bf602fe4cb35b1c505e2828f431b9ca8a77581
jlyajure <jlyt1710@gmail.com>
2026-02-18 16:37:05 -0400

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD-Su-Clave-Secreta-Aqui", 
+  authDomain: "familia-app.firebaseapp.com",
+  projectId: "familia-app",
+  storageBucket: "familia-app.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
