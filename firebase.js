60fbf6f7da02707d12b30239836de7a5c32c0223
jlyajure <jlyt1710@gmail.com>
2026-02-18 17:03:47 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
+// Tu configuración REAL de la Familia Yajure
 const firebaseConfig = {
-  apiKey: "AIzaSyD-Su-Clave-Secreta-Aqui", 
-  authDomain: "familia-app.firebaseapp.com",
-  projectId: "familia-app",
-  storageBucket: "familia-app.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
+  apiKey: "AIzaSyBwBq4gLgv4DSfUidzUuC7Irmvj_4pCTtI",
+  authDomain: "familia-yajure-app.firebaseapp.com",
+  projectId: "familia-yajure-app",
+  storageBucket: "familia-yajure-app.firebasestorage.app",
+  messagingSenderId: "692035727386",
+  appId: "1:692035727386:web:dfa3e39a481d56368a61a3",
+  measurementId: "G-PLRGRQT36L"
 };
 
+// Iniciamos la conexión
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
